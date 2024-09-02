480883bed47603c6752a49550318cbf90c4b676f
wylouuu <lojokowilly@gmail.com>
2024-09-03 01:20:58 +0700

    implement hostname on the run dev package.json

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
 	return (
-		<main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-red-600'>
+		<main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
 			<div className='max-w-7xl w-full'>
 				<Hero />
 			</div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { TextGenerateEffect } from './ui/TextGenerateEffect';
 
 const Hero = () => {
 	return (
-		<div className='pb-20 pt-36 '>
+		<section className='pb-20 pt-36 '>
 			<div>
 				<Spotlight
 					className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen'
@@ -18,7 +18,7 @@ const Hero = () => {
 				<Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
 			</div>
 
-			<div className='h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0'>
+			<div className='h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0'>
 				<div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
 			</div>
 
@@ -46,7 +46,7 @@ const Hero = () => {
 					</a>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
