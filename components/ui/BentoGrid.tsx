8f9d96b8d4216bcd33c10bbfb0a0a88bce865c7d
wylouuu <lojokowilly@gmail.com>
2024-09-03 22:43:34 +0700

    implement fix on bento grid wrapper

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,7 +10,7 @@ export const BentoGrid = ({
 	return (
 		<div
 			className={cn(
-				'grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
+				'grid md:grid-cols-6 grid-cols-1  lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
 				className
 			)}>
 			{children}
@@ -45,7 +45,8 @@ export const BentoGridItem = ({
 			)}
 			style={{
 				background: 'rgb(4,7,29)',
-				backgroundColor: 'gradient...',
+				backgroundColor:
+					'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
 			}}>
 			<div className='group-hover/bento:translate-x-2 transition duration-200'>
 				<div className='font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2'>
