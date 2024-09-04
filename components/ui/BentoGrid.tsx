4e54eb153836f802479c4a74617db03b4111db99
wylouuu <lojokowilly@gmail.com>
2024-09-04 17:29:23 +0700

    implement globe and background gradient in bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import { cn } from '@/lib/utils';
+import { BackgroundGradientAnimation } from './GradientBg';
+import { GlobeDemo } from './GridGlobe';
 
 export const BentoGrid = ({
 	className,
@@ -40,7 +42,7 @@ export const BentoGridItem = ({
 	return (
 		<div
 			className={cn(
-				'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
+				'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
 				className
 			)}
 			style={{
@@ -48,12 +50,61 @@ export const BentoGridItem = ({
 				backgroundColor:
 					'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
 			}}>
-			<div className='group-hover/bento:translate-x-2 transition duration-200'>
-				<div className='font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2'>
-					{title}
+			<div className={`${id === 6} && flex justify-center h-full`}>
+				<div className='h-full w-full absolute'>
+					{img && (
+						<img
+							src={img}
+							alt={img}
+							className={cn(imgClassName, 'object-cover object-center')}
+						/>
+					)}
 				</div>
-				<div className='font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300'>
-					{description}
+				<div
+					className={`absolute right-0 -bottom-5 ${
+						id === 5 && 'w-full opacity-80'
+					}`}>
+					{spareImg && (
+						<img
+							src={spareImg}
+							alt={spareImg}
+							className={'object-cover object-center w-full h-full'}
+						/>
+					)}
+				</div>
+				{id === 6 && (
+					<BackgroundGradientAnimation>
+						<div className='absolute z-50 flex items-center justify-center text-white font-bold' />
+					</BackgroundGradientAnimation>
+				)}
+
+				<div
+					className={cn(
+						titleClassName,
+						'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+					)}>
+					<div className='font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10'>
+						{description}
+					</div>
+					<div className='font-sans font-bold text-lg lg:text-3xl max-w-96 z-10'>
+						{title}
+					</div>
+
+					{id === 2 && <GlobeDemo />}
+
+					{id === 3 && (
+						<div className='flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2'>
+							<div className='flex flex-col gap-3 lg:gap-8'>
+								{['React.js', 'Next.js', 'Typescript'].map((item) => (
+									<span
+										key={item}
+										className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opactiy-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]'>
+										{item}
+									</span>
+								))}
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
